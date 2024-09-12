dfb07d8bbd5025c2c8aacfc7ea027858bb2c22f8
Quy Duong Nguyen <qnguye11@student.ubc.ca>
2024-09-11 20:59:21 -0700

    completed 3

--- libtempvolts.h
+++ libtempvolts.h
@@ -7,3 +7,22 @@
  * Every time you call the function, it will return the next voltage reading
  */
 float get_voltage();
+float io_read_temperature();
+
+// Calculates the average temperature
+float average_temperature() {
+    float total_temperature = 0.0;
+
+    // read temperature NUM_VOLTAGE_READINGS times
+    for (int i = 0; i < NUM_VOLTAGE_READINGS; i++) {
+        // read the temperature using the io_read_temperature() function
+        float temperature = io_read_temperature();
+        total_temperature += temperature;
+    }
+
+    // calculate the average temperature
+    float average_temp = total_temperature / NUM_VOLTAGE_READINGS;
+
+    // return the average temperature
+    return average_temp;
+}
